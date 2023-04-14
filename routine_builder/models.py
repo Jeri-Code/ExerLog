a024d011d493f7d69c8958fcd6880c4f1d932d09
Jeri-Code <jerichotimbol6@gmail.com>
2023-04-13 20:59:44 -0700

    Migrated /cleaned  PgSQL table  into models.

--- routine_builder/models.py
+++ routine_builder/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Exercises(models.Model):
+    id = models.IntegerField(blank=True, null=False,primary_key= True)
+    name = models.CharField(blank=True, null=True)
+    body_part = models.CharField(blank=True, null=True)
+    equipment = models.CharField(blank=True, null=True)
+    gif_url = models.CharField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'exercises'
+
+    def __str__(self):
+        return self
+
+

d283f99e2af05c0be84d3a486004f408e907f48c
Jeri-Code <jerichotimbol6@gmail.com>
2023-04-14 13:35:33 -0700

    Added url router to API GET exercises

---
+++ api/urls.py
@@ -0,0 +1,16 @@
+# myapi/urls.py
+from django.urls import include, path
+from rest_framework import routers
+from . import views
+
+#The REST Framework router will make sure our requests end up at the right resource dynamically. 
+# If we add or delete items from the database, the URLs will update to match
+router = routers.DefaultRouter()
+router.register(r'exercises', views.ExercisesViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]

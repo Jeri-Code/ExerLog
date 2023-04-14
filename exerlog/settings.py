6f47b5427cce4732d1a2bf39851510bc6741a264
Jeri-Code <jerichotimbol6@gmail.com>
2023-04-14 12:56:13 -0700

    Created django api app

--- exerlog/settings.py
+++ exerlog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'routine_builder',
+    'routine_builder'
+    'api',
 ]
 
 MIDDLEWARE = [

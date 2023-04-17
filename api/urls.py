cee5dc97cdcb180073d7842f065f2e1389e4fe54
Jeri-Code <jerichotimbol6@gmail.com>
2023-04-17 13:19:11 -0700

    Revert back to viewset for efficiency

--- api/urls.py
+++ api/urls.py
@@ -5,13 +5,11 @@ from . import views
 
 #The REST Framework router will make sure our requests end up at the right resource dynamically. 
 # If we add or delete items from the database, the URLs will update to match
-'''
 router = routers.DefaultRouter()
 router.register(r'exercises', views.ExercisesViewSet)
-'''
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', views.getExercises),
+    path('', include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from .serializers import ExercisesSerializer
 from routine_builder.models import Exercises
 
-@api_view(['GET'])
-def getExercises(request):
-    all_exercises =  Exercises.objects.all()
-    serializer = ExercisesSerializer(all_exercises, many = True)
-    return Response(serializer.data)
-# Create your views here.
+
+class ExercisesViewSet(viewsets.ModelViewSet):
+    queryset = Exercises.objects.all()
+    serializer_class = ExercisesSerializer
+    
+    def retrieve(self, request, pk=None):
+        exercise = get_object_or_404(self.queryset, pk=pk)
+        serializer = ExercisesSerializer(exercise)
+        return Response(serializer.data)
+    
+

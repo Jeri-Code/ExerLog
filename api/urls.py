cbe137100ff0181169337f90e821cd0ef5b564d3
Jeri-Code <jerichotimbol6@gmail.com>
2023-04-28 18:02:12 -0700

    improved root API view for easy URL accessibility

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,44 @@
 # myapi/urls.py
-from django.urls import include, path
-from rest_framework import routers
+from django.urls import path, include
 from . import views
+from users.views import RegisterAPI
+from users.views import LoginAPI
+from knox import views as knox_views
+from rest_framework.routers import DefaultRouter
 
-#The REST Framework router will make sure our requests end up at the right resource dynamically. 
-# If we add or delete items from the database, the URLs will update to match
-router = routers.DefaultRouter()
-router.register(r'exercises', views.ExercisesViewSet)
+class RouterWithExtraItemsInRoot(DefaultRouter):
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+    extra_urls = {'register': 'register',
+                  'login':'login',
+                  'logout':'logout',
+                  'logoutall':'logoutall',
+                 
+                  }
+
+    def get_api_root_view(self, api_urls=None):
+        api_root_dict = {}
+        list_name = self.routes[0].name
+        for prefix, viewset, basename in self.registry:
+            api_root_dict[prefix] = list_name.format(basename=basename)
+
+        api_root_dict.update(self.extra_urls)  # Add extra urls
+        return self.APIRootView.as_view(api_root_dict=api_root_dict)
+
+router = RouterWithExtraItemsInRoot()
+router.register (r'exercises', views.ExercisesViewSet)
+router.extra_urls['url/path'] = 'view_name'
+
+
+
+
+
+
 urlpatterns = [
+    path('login/', LoginAPI.as_view(), name='login'),
+    path('logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
+    path('register/', RegisterAPI.as_view(), name = 'register'),
     path('', include(router.urls)),
+
 ]
+

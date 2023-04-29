8d81455b3d48c3d1bfc425cc5c786694e18e3cd0
Jeri-Code <jerichotimbol6@gmail.com>
2023-04-28 18:17:41 -0700

    added routine and workout database model

--- api/serializers.py
+++ api/serializers.py
@@ -5,4 +5,7 @@ from routine_builder.models import Exercises
 class ExercisesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exercises
-        fields = '__all__'
+        fields = ('name', 'muscle_group','body_part','equipment', 'gif_url'
+                  
+                  )
+        read_only_fields = fields

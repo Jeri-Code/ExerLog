2908ff03b539e05fedae904d6299564994013692
Jeri-Code <jerichotimbol6@gmail.com>
2023-04-28 18:03:50 -0700

    Implemented exercise views filtering

--- api/views.py
+++ api/views.py
@@ -1,17 +1,31 @@
 from rest_framework import viewsets
-from django.shortcuts import get_object_or_404
+from django import forms
+from django.shortcuts import render
 from rest_framework.response import Response
 from .serializers import ExercisesSerializer
 from routine_builder.models import Exercises
+from django_filters import rest_framework as filters
 
 
+
+class ExerciseFilter(filters.FilterSet):
+    name = filters.CharFilter(lookup_expr = 'istartswith')
+    body_part = filters.CharFilter(lookup_expr = 'istartswith')
+    equipment = filters.CharFilter(lookup_expr = 'istartswith')
+    muscle_group = filters.CharFilter(lookup_expr = 'istartswith')
+
+    '''
+    muscle_group = filters.ModelChoiceFilter(
+        queryset=Exercises.objects.all(),
+        empty_label="All Categories",
+        label="Categories",
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        )
+
+'''
 class ExercisesViewSet(viewsets.ModelViewSet):
     queryset = Exercises.objects.all()
     serializer_class = ExercisesSerializer
-    
-    def retrieve(self, request, pk=None):
-        exercise = get_object_or_404(self.queryset, pk=pk)
-        serializer = ExercisesSerializer(exercise)
-        return Response(serializer.data)
+    filterset_class = ExerciseFilter
     
 

6a601e42ea6d6537f268ed1bb7fad48078188d07
Jeri-Code <jerichotimbol6@gmail.com>
2023-05-04 23:28:15 -0700

    authenticated views. added routine view

--- api/views.py
+++ api/views.py
@@ -2,9 +2,11 @@ from rest_framework import viewsets
 from django import forms
 from django.shortcuts import render
 from rest_framework.response import Response
-from .serializers import ExercisesSerializer
-from routine_builder.models import Exercises
+from .serializers import ExercisesSerializer, RoutineSerializer
+from routine_builder.models import Exercises, Routine
 from django_filters import rest_framework as filters
+from rest_framework.permissions import IsAuthenticated
+
 
 
 
@@ -24,8 +26,13 @@ class ExerciseFilter(filters.FilterSet):
 
 '''
 class ExercisesViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     queryset = Exercises.objects.all()
     serializer_class = ExercisesSerializer
     filterset_class = ExerciseFilter
-    
 
+class RoutineViewset(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    http_method_names = ['get', 'post', 'delete']
+    serializer_class = RoutineSerializer
+    queryset = Routine.objects.all()

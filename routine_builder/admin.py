3f55256d46a534de3ff32cecb445d8613f645001
Jeri-Code <jerichotimbol6@gmail.com>
2023-05-10 15:44:35 -0700

    admin page update and registration

--- routine_builder/admin.py
+++ routine_builder/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
+from .models import Exercises, Workout,Routine
+from rest_framework import serializers
 
-from .models import Exercises
 # Register your models here.
 class ExerciseAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'body_part')
 
 admin.site.register(Exercises, ExerciseAdmin)
+
+class WorkoutAdmin(admin.ModelAdmin):
+    list_display = ('id', 'reps')
+
+admin.site.register(Workout, WorkoutAdmin)
+
+class RoutineAdmin(admin.ModelAdmin):
+    list_display = ('routine_name','id')
+
+admin.site.register(Routine, RoutineAdmin)

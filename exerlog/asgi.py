d00384a8de6674a26637285a475b9c47a3ce8779
Brandon Tran <tranbrandon@live.com>
2023-05-09 17:04:06 -0700

    Fixed comments

--- exerlog/asgi.py
+++ exerlog/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for exerlog project.
+ASGI config for ExerLog project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exerlog.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "exerlog.settings")
 
 application = get_asgi_application()
--- exerlog/wsgi.py
+++ exerlog/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for exerlog project.
+WSGI config for ExerLog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exerlog.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "exerlog.settings")
 
 application = get_wsgi_application()

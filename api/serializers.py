e7b75dba33bc7ebea77ad193fe8f5b666d0b9a7e
Jeri-Code <jerichotimbol6@gmail.com>
2023-05-10 15:43:21 -0700

    Workout and Routine writable nested serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,17 +1,40 @@
 from rest_framework import serializers
-
-from routine_builder.models import Exercises, Routine
+from django.contrib.auth.models import User
+from routine_builder.models import Exercises,Workout,Routine
 
 class ExercisesSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Exercises
-        fields = ('name', 'muscle_group','equipment', 'gif_url'
-                  
-                  )
-        read_only_fields = fields
+        fields = ('id','name', 'muscle_group','equipment', 'gif_url')
 
-class RoutineSerializer(serializers.ModelSerializer):
-
+    
+    
+class WorkoutSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source = 'exercise.name',read_only = True)
+    body_part = serializers.CharField(source = 'exercise.body_part', read_only = True)
+    gif_url = serializers.CharField(source = 'exercise.gif_url',read_only = True)
+    
     class Meta:
+        model = Workout
+        fields = ['exercise','name','body_part','sets','reps','duration','gif_url','routine']
+
+
+
+    
+class RoutineSerializer(serializers.ModelSerializer):
+    routine_workouts = WorkoutSerializer(many = True, read_only = True)
+    class Meta:     
         model = Routine
-        fields = ['name', 'day', 'user_id']
+        fields = ['id','user','routine_name','day','routine_workouts']
+
+    
+def create(self, validated_data):
+    request_user = self.context['request'].user
+    instance = Routine.objects.create(user=request_user)
+
+    
+    return instance
+
+
+

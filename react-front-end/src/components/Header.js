302427ff83e68395e39178188fa6da6838c6e0e7
Brandon Tran <tranbrandon@live.com>
2023-05-15 03:38:11 -0700

    Updated images and finished footer + homepage

--- react-front-end/src/components/Header.js
+++ react-front-end/src/components/Header.js
@@ -1,50 +1,48 @@
-import React from "react";
-import { Component } from "react";
+import React, { useState } from "react";
 import GymLogo from "../img/favicon.ico";
 import { Link } from "react-router-dom";
 
-export default class Header extends Component {
-  state = { clicked: false };
-  handleClick = () => {
-    this.setState({ clicked: !this.state.clicked });
+const Header = () => {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!clicked);
   };
-  render() {
-    return (
-      <div className={`box ${this.state.clicked ? "active" : ""}`} id="navbar">
-        <nav className="navbar">
-          <a href="/Homepage" className="logo">
-            <img src={GymLogo} alt="Gym Page Logo" aria-label="Home" />
-          </a>
-          <div className="menu">
-            <a href="/WeeklyPlanner" className="nav-item" aria-label="Login">
-              Weekly Planner
-            </a>
-            <a href="/Account" className="nav-item" aria-label="Account">
-              Account
-            </a>
-            <a href="/FAQ" className="nav-item" aria-label="FAQ">
-              FAQ
-            </a>
-            <a href="/AboutUs" className="nav-item" aria-label="About Us">
-              About Us
-            </a>
-          </div>
-          <div className="auth-buttons">
-            <a href="/SignUp" className="auth-button" aria-label="Sign Up">
-              Sign Up
-            </a>
-            <a href="/Login" className="auth-button" aria-label="Login">
-              Login
-            </a>
-          </div>
-          <div id="mobile" onClick={this.handleClick}>
-            <i
-              id="bar"
-              className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
-            ></i>
-          </div>
-        </nav>
-      </div>
-    );
-  }
-}
+
+  return (
+    <header className={`box ${clicked ? "active" : ""}`} id="navbar">
+      <nav className="navbar">
+        <Link to="/Homepage" className="logo">
+          <img src={GymLogo} alt="Gym Page Logo" aria-label="Home" />
+        </Link>
+        <div className="menu">
+          <Link to="/WeeklyPlanner" className="nav-item" aria-label="Weekly Planner">
+            Weekly Planner
+          </Link>
+          <Link to="/Account" className="nav-item" aria-label="Account">
+            Account
+          </Link>
+          <Link to="/FAQ" className="nav-item" aria-label="FAQ">
+            FAQ
+          </Link>
+          <Link to="/AboutUs" className="nav-item" aria-label="About Us">
+            About Us
+          </Link>
+        </div>
+        <div className="auth-buttons">
+          <Link to="/SignUp" className="auth-button" aria-label="Sign Up">
+            Sign Up
+          </Link>
+          <Link to="/Login" className="auth-button" aria-label="Login">
+            Login
+          </Link>
+        </div>
+        <div id="mobile" onClick={handleClick}>
+          <i id="bar" className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- react-front-end/src/components/Homepage.js
+++ react-front-end/src/components/Homepage.js
@@ -1,12 +1,24 @@
 import React from "react";
+import Dumbbells from "../img/dumbbells.png";
+import Treadmill from "../img/treadmill.png";
 
-export default function Homepage(props) {
-	return (
-		<div className="welcome">
-			<div className="title">
-				<h2> ExerLog </h2>
-			</div>
-			<div className="legend">Gym Routine planning done simple.</div>
-		</div>
-	);
-}
+const Homepage = (props) => {
+  return (
+    <div className="welcome">
+      <img src={Treadmill} alt="Black treadmill" className="treadmill" />
+      <h1 className="title">
+        ExerLog
+        <button className="join-now-btn">Join Now</button>
+      </h1>
+      <div className="second-title">
+        Gym Routines done simple.
+        <img src={Dumbbells} alt="Two dumbbells stacked on top of each other" className="dumbbells" />
+      </div>
+      <div className="sign-up">
+        Already signed up? <a href="/Login">Click Here</a>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;

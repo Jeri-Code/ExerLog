8cca82f53d7417d13e4bbd5ffdcd3c3c5fbff0e1
Jeri-Code <jerichotimbol6@gmail.com>
2023-05-28 20:44:33 -0700

    Dashboard view for auth test and misc configs

--- frontend/src/Reducer.js
+++ frontend/src/Reducer.js
@@ -1,10 +1,13 @@
 // frontend/src/Reducer.js
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
+import { loginReducer } from "./components/login/LoginReducer";
+
 
 const createRootReducer = history =>
   combineReducers({
     router: connectRouter(history),
+    auth: loginReducer
   });
 
 export default createRootReducer;
